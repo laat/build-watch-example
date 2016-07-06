b377dfe209901aeb429868a5d2eb61f037b23896
Sigurd Fosseng <sigurd.fosseng@sparebank1.no>
2016-07-06 12:54:42 +0200

    watch web

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 watchman-make \
 	--make="npm run" \
+	-p 'packages/**' -t "build:web" \
 	-p 'packages/server/src/**' -t "build:server" \
 	-p 'packages/shared/src/**' -t "build:shared" \
